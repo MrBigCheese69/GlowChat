7fdcfabeb80566fc497b0fba3c7a36748c993b46
MrBigCheese69 <vmw951753@gmail.com>
2025-05-13 19:21:06 -0400

    fuck backend shi9t

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
 const wss = new WebSocket.Server({ server });
 
 const port = process.env.PORT || 3000;
-const uri = "mongodb+srv://explodingcreper91:j3zd87jue9YASMjP@glowchat.jh5jzxu.mongodb.net/?retryWrites=true&w=majority&appName=GlowChat";
+const uri = "mongodb+srv://explodingcreper91:j3zd87jue9YASMjP@glowchat.jh5jzxu.mongodb.net/?retryWrites=true&w=majority&appName=GlowChat&tls=true&tlsAllowInvalidCertificates=true";
 const client = new MongoClient(uri);
 
 async function run() {
