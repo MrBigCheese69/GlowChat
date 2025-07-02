2c1a00fc48b8dff4c7bc4edda3ab19093ed2c5e7
MrBigCheese69 <vmw951753@gmail.com>
2025-07-02 02:34:31 -0400

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"; // ✅ Add this
 
 const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "authDomain",
-  databaseURL: "databaseURL",
-  projectId: "projectId",
-  storageBucket: "storageBucket",
-  messagingSenderId: "messagingSenderId",
-  appId: "appId",
-  measurementId: "measurementId"
+  apiKey: "AIzaSyCmPy8Uv_TDH5T3-2sApK0yQ4vchtrNwDU",
+  authDomain: "glowchat-a5426.firebaseapp.com",
+  databaseURL: "https://glowchat-a5426-default-rtdb.firebaseio.com",
+  projectId: "glowchat-a5426",
+  storageBucket: "glowchat-a5426.firebasestorage.app",
+  messagingSenderId: "399090889166",
+  appId: "1:399090889166:web:ed05bfeff8d8a33d447b9f",
+  measurementId: "G-68VYC3XVG5"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
