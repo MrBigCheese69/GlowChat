5291490d8d2901a2b6e063465fe26c9b7fd86b4c
MrBigCheese69 <vmw951753@gmail.com>
2025-07-02 01:28:05 -0400

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmPy8Uv_TDH5T3-2sApK0yQ4vchtrNwDU",
-  authDomain: "glowchat-a5426.firebaseapp.com",
-  databaseURL: "https://glowchat-a5426-default-rtdb.firebaseio.com",
-  projectId: "glowchat-a5426",
-  storageBucket: "glowchat-a5426.firebasestorage.app",
-  messagingSenderId: "399090889166",
-  appId: "1:399090889166:web:ed05bfeff8d8a33d447b9f",
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: " databaseURL",
+  projectId: " projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
   measurementId: "G-68VYC3XVG5"
 };
 
